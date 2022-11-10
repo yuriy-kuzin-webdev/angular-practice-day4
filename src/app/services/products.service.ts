81d5aae085e5fc38983f4c413b80ce82d6038159
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-11-10 21:23:09 +0200

    added limit

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { IProduct } from '../models/product';
 import { Observable } from 'rxjs';
 @Injectable({
@@ -9,6 +9,8 @@ export class ProductService {
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>('https://fakestoreapi.com/products');
+    return this.http.get<IProduct[]>('https://fakestoreapi.com/products', {
+      params: new HttpParams().append('limit', 5),
+    });
   }
 }
